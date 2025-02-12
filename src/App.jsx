f19033ab473a616165961b811025ce02a0d6558c
unext <dev@unext.de>
2025-02-12 19:54:40 +0100

    v0.1

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import HomeLayout from './pages/HomeLayout';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <HomeLayout />,
+  },
+  {
+    path: '/about',
+    element: (
+      <div>
+        <h2>about page</h2>
+      </div>
+    ),
+  },
+]);
+
+const App = () => {
+  return <RouterProvider router={router} />;
+};
+export default App;
